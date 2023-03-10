646fc0f14f94f021271ff95d6e20a284962882f7
mohamedhachguer <96432303+HachguerMohamed@users.noreply.github.com>
2023-03-10 17:10:20 +0100

    Initial Commit

---
+++ GestionEnsaTanger/Etudiant.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionEnsaTanger
+{
+    internal class Etudiant 
+    {
+        public string Code, nom, prenom, niveau, code_Fil;
+    }
+}

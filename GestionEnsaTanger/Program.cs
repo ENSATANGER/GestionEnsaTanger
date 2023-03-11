d01735f79ce1c012980bd24083a46a594116063b
Mouad Erghout <mouad.erghout.5@gmail.com>
2023-03-11 11:24:31 +0100

    com1

--- GestionEnsaTanger/Program.cs
+++ GestionEnsaTanger/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DB;
 
 namespace GestionEnsaTanger
 {
@@ -10,7 +11,26 @@ namespace GestionEnsaTanger
     {
         static void Main(string[] args)
         {
-            new UI().ShowDialog();
+            Filiere F1 = new Filiere("GINF", "genie informatique");
+            Filiere F2 = new Filiere("GSTR", "genie systemes reseaux et telecommunication");
+            Filiere F3 = new Filiere("GINF", "genie informatique");
+            Filiere F4 = new Filiere("GINF", "genie informatique");
+            Filiere F5 = new Filiere("GINF", "genie informatique");
+
+            /*F1.save();
+            F2.save();
+            F3.save();
+            F4.save();
+            F5.save();*/
+
+            /*Filiere F = F1.find();
+            F1 = Filiere.find<Filiere>(2);
+            F2 = F;*/
+
+            List<dynamic> ListeFilieres = F1.All();
+
+            foreach(dynamic item in ListeFilieres)
+                Console.WriteLine(item+"\n");
         }
     }
 }

ad8de52d15ffdebbd6d4defb6ea3a8d2f5771609
Louay <louay.alami.ouahabi@yosobox.com>
2023-03-15 15:33:56 +0100

    Update Program.cs

--- GestionEnsaTanger/Program.cs
+++ GestionEnsaTanger/Program.cs
@@ -13,16 +13,18 @@ namespace GestionEnsaTanger
         {
             /*new MDI().ShowDialog();*/
 
-            Module m = new Module("c5", "d1", "n1", "s1","c1");
-            Module.Create(m);
-            
-            List<Module> modules = Module.Read();
+            Module m = new Module("c5", "d2", "n2", "s1","c1");
+/*            Module.Create(m);
+*/            
+            /*List<Module> modules = Module.Read();
                 foreach (Module module in modules)
                 {
                     Console.WriteLine(module.code);
-                }
-            
+                }*/
 
+            Module.Update(m);
+/*            Module.Delete(m.code);
+*/
             Console.ReadKey();  
         }
     }

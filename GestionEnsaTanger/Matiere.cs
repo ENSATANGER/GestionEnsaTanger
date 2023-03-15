b542e0aa373dfe70ee3347825715c636118d8857
mohamedhachguer <96432303+HachguerMohamed@users.noreply.github.com>
2023-03-15 23:16:41 +0100

    fetching matieres

---
+++ GestionEnsaTanger/Matiere.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DB;
+
+namespace GestionEnsaTanger
+{
+    internal class Matiere : Model
+    {
+        string Code, Designation, Code_module;
+        Double vh;
+
+        public Matiere() { }
+        
+        public string code
+        {
+            get { return Code; }
+            set { Code = value; }
+        }
+
+        public string designation
+        {
+            get { return Designation; }
+            set { Designation = value; }
+        }
+
+        public string code_module
+        {
+            get { return Code_module; }
+            set { Code_module = value; }
+        }
+
+        public Double VH
+        {
+            get { return vh; }
+            set{ vh = value; }
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + "code: " + code;
+        }
+
+    }
+}
--- GestionEnsaTanger/Program.cs
+++ GestionEnsaTanger/Program.cs
@@ -13,16 +13,37 @@ namespace GestionEnsaTanger
         {
             new MDI().ShowDialog();
             /*Eleve eleve = new Eleve();
-            eleve.code = "BBB";
-            Dictionary<string,object> map = new Dictionary<string,object>();
+            eleve.code = "AAA";
+            Dictionary<string, object> map = new Dictionary<string, object>();
             map.Add("code", eleve.code);
             List<object> list = eleve.Select(map);
             if (list.Count == 0)
-                Console.WriteLine("vide");
-            foreach(object item in list)
+                Console.WriteLine("vide");*/
+            /*foreach (object item in list)
             {
-                
+
                 Console.WriteLine(item.ToString());
+            }*/
+           /* eleve = (Eleve)list.First();
+            Dictionary<string, object> map2 = new Dictionary<string, object>();
+            map2.Add("code_fil", eleve.code_fil);
+            map2.Add("niveau", eleve.niveau);*/
+            /*foreach(KeyValuePair<string,object> e in map2)
+            {
+                Console.WriteLine(e.Key+" "+e.Value);
+            }*/
+            /*Module m = new Module();
+            List<object> list2 = m.Select(map2);
+            m = (Module)list2.First();
+            map.Clear();
+            map.Add("code_module", m.code);
+            Matiere matiere = new Matiere();
+            list2.Clear();
+            list2 = matiere.Select(map);
+            foreach (object obj in list2)
+            {
+                matiere = (Matiere)obj;
+                Console.WriteLine(matiere);
             }*/
             /*if (eleve.find() == null)
             {

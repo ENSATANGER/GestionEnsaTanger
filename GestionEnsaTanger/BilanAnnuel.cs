4efd6d59b04dab52a48a72d86759e7ea73f2a767
Louay <louay.alami.ouahabi@yosobox.com>
2023-03-17 03:11:12 +0100

    Update BilanAnnuel.cs

--- GestionEnsaTanger/BilanAnnuel.cs
+++ GestionEnsaTanger/BilanAnnuel.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +20,10 @@ namespace GestionEnsaTanger
             InitializeComponent();
         }
 
+
         private void filiere_SelectedIndexChanged(object sender, EventArgs e)
         {
-            
+            elevlist();
         }
 
         private void niveau_SelectedIndexChanged(object sender, EventArgs e)
@@ -30,27 +32,25 @@ namespace GestionEnsaTanger
 
             if (niveau.SelectedItem != null && niveau.SelectedItem.ToString() != "AP1" && niveau.SelectedItem.ToString() != "AP2")
             {
-                Filiere fil = new Filiere();
-                List<dynamic> filList = fil.All();
-                System.Object[] itemObjects = new System.Object[filList.Count];
-                int i = 0;
-                foreach (Filiere frow in filList)
-                {
-                    itemObjects[i] = frow.code;
-                    i++;
-                }
-                filiere.Items.AddRange(itemObjects);
+                fillist();
             }
+            else
+            {
+                filiere.Items.Clear();
+                filiere.Text= "باقي صغير";
+            }
+            elevlist();
+            
         }
 
         private void etudiant_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            
         }
 
         private void rechercher_Click(object sender, EventArgs e)
         {
-
+            
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
@@ -67,6 +67,34 @@ namespace GestionEnsaTanger
         {
             
         }
-
+        private void fillist()
+        {
+            filiere.Text = "filiere";
+            Filiere fil = new Filiere();
+            List<dynamic> filList = fil.All();
+            System.Object[] itemObjects = new System.Object[filList.Count];
+            int i = 0;
+            foreach (Filiere f1 in filList)
+            {
+                itemObjects[i] = f1.code;
+                i++;
+            }
+            filiere.Items.AddRange(itemObjects);
+        }
+        private void elevlist()
+        {
+            etudiant.Items.Clear();
+            Eleve el = new Eleve();
+            List<dynamic> elevelist = el.All();
+            elevelist = elevelist.Where(e => e.niveau == niveau.Text && e.filiere == filiere.Text).ToList();
+            System.Object[] itemObjects2 = new System.Object[elevelist.Count];
+            int j = 0;
+            foreach (Eleve e1 in elevelist)
+            {
+                itemObjects2[j] = e1.nom;
+                j++;
+            }
+            etudiant.Items.AddRange(itemObjects2);
+        }
     }
 }
